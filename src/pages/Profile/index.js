4d7e65e0ea8c3179e550a87b83bc9836d5352345
Jhonatanvasc <jhonatanvasconcelosrj@hotmail.com>
2023-11-15 10:34:01 -0300

    barber

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
+import AvatarInput from './AvatarInput';
+
+export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Sua nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua nova senha"
+        />
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do goBarber</button>
+    </Container>
+  );
+}
---
+++ src/pages/SingUp/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .required('A senha é Obrigatória')
+    .min(6, 'Mínimo 6 caracteres'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="logo gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
